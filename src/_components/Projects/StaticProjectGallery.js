e48bbe677f1b7a90d52fbc03c557cf1ec0e5c5ae
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-24 14:04:56 -0700

    change link to myhouse

--- src/_components/Projects/StaticProjectGallery.js
+++ src/_components/Projects/StaticProjectGallery.js
@@ -35,7 +35,7 @@ const StaticProjectGallery = () => {
           <div className="btn-group">
             <a
               className="app-btn"
-              href="https://myhouse-app.now.sh/"
+              href="https://www.joinmyhouse.com/"
               target="_blank"
               rel="noopener noreferrer"
             >
